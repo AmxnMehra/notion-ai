7c2e6c482b71447dcf1a870b50cd6320b1c7ada0
AmxnMehra <amanmehra0112233@gmail.com>
2025-08-08 20:53:20 +0530

    added new Document button

--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
+import { useTransition } from "react";
+import { createNewDocument } from "@/actions/actions";
 
 function NewDocumentButton() {
-  return <Button>New Document</Button>;
-}
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
 
+  const handleCreateNewFunction = () => {
+    // create a new document
+    startTransition(async () => {
+      const { docId } = await createNewDocument();
+      router.push(`/doc/${docId}`);
+    });
+  };
+  return (
+    <Button onClick={handleCreateNewFunction} disabled={isPending}>
+      {isPending ? "Creating..." : "New Document"}
+    </Button>
+  );
+}
 export default NewDocumentButton;
